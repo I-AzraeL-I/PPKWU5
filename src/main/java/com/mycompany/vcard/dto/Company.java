d1cf9450378a2638cde380ebf26ce56f838e6872
I-AzraeL-I <qaraqan70@gmail.com>
2021-12-09 13:07:30 +0100

    Implement Company class equals and hashcode to remove duplicates

--- src/main/java/com/mycompany/vcard/dto/Company.java
+++ src/main/java/com/mycompany/vcard/dto/Company.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
@@ -19,4 +21,17 @@ public class Company {
     private Contact contact;
     private String companyHash;
     private String tradeHash;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Company company = (Company) o;
+        return name.equals(company.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
